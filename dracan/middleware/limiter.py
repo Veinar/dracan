b866f7992e3c04b315f31be4ef4c54d0f9aac5ff
Veinar <conra.kurek@gmail.com>
2024-10-22 16:16:46 +0200

    Added basic logging logic - minor fix

--- dracan/middleware/limiter.py
+++ dracan/middleware/limiter.py
@@ -1,15 +1,17 @@
-# dracan/middleware/limiter.py
-
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import warnings
 
-def create_limiter(app, rules_config):
+def create_limiter(app, rules_config,):
     """
     Initialize and apply the rate limiter if limiting is enabled.
     :param app: The Flask app instance.
     :param rules_config: The configuration that contains limiting rules.
     :return: Limiter object or None if limiting is disabled.
     """
+    # Suppress the specific warning about in-memory storage
+    warnings.filterwarnings("ignore", message="Using the in-memory storage for tracking rate limits as no storage was explicitly specified")
+
     if rules_config.get("limiting_enabled", False):
         rate_limit = rules_config.get("rate_limit", "10 per minute")
         limiter = Limiter(
@@ -17,8 +19,8 @@ def create_limiter(app, rules_config):
             app=app,
             default_limits=[rate_limit]
         )
-        print(f"Rate limiting enabled with limit: {rate_limit}")
+        app.logger.info(f"Rate limiting enabled with limit: {rate_limit}")
         return limiter
     else:
-        print("Rate limiting is disabled")
+        app.logger.info("Rate limiting is disabled")
         return None
