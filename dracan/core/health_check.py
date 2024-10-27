018aafee3cacbb1aa1e39f510e99431c9e2d2df1
Veinar <conrad.kurek@gmail.com>
2024-10-27 22:15:30 +0100

    Add initial solution for #7 HealthCheck

---
+++ dracan/core/health_check.py
@@ -0,0 +1,26 @@
+import os
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            # Send a 200 OK response
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            
+            # Write JSON response
+            response = json.dumps({"status": "running"})
+            self.wfile.write(response.encode())
+        else:
+            # Send a 404 Not Found response for other paths
+            self.send_response(404)
+            self.end_headers()
+
+def run_health_check_server():
+    health_port = int(os.getenv("HEALTHCHECK_PORT", 9000))
+    server_address = ('', health_port)
+    httpd = HTTPServer(server_address, HealthCheckHandler)
+    print(f"Health check server running on port {health_port}")
+    httpd.serve_forever()
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
+from threading import Thread
 from dracan.core.app_factory import create_app
+from dracan.core.health_check import run_health_check_server
 
 app = create_app()
 
 if __name__ == '__main__':
+    # Check if health check should be disabled
+    healthcheck_disabled = os.getenv("HEALTHCHECK_DISABLED", "false").lower() == "true"
+
+    if not healthcheck_disabled:
+        # Start the health check server in a separate thread
+        health_thread = Thread(target=run_health_check_server)
+        health_thread.daemon = True  # Ensures the thread stops when the main process exits
+        health_thread.start()
+    else:
+        print("Healthcheck not running, disabled by env variable.")
+
     # Run the Flask app
     app.run(host='0.0.0.0', port=5000)
